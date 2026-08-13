1d6f6f4fb63bff083ca14a995e974a750c00b055
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-13 23:36:33 +0300

    removed images from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,24 +6,10 @@ function Navbar() {
     <nav className={styles.navbar}>
       <div className={styles.navContainer}>
 
-        {/* Coffee shop brand */}
         <Link to="/" className={styles.brand}>
-          <img
-            src="https://images.unsplash.com/photo-1447933601403-0c6688de566e"
-            alt="Coffee beans"
-            className={styles.bean}
-          />
-
-          <span>Brew Showcase</span>
-
-          <img
-            src="https://images.unsplash.com/photo-1559056199-641a0ac8b55e"
-            alt="Coffee beans"
-            className={styles.bean}
-          />
+          Brew Showcase
         </Link>
 
-        {/* Navigation links */}
         <div className={styles.links}>
           <Link to="/" className={styles.link}>
             Home
