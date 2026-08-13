25723e6241838e13094fc6034f56953d58ad5c16
Abdikalolo <abdizeabdi18@gmail.com>
2026-08-13 17:02:24 +0300

    added images to the landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,43 @@
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <Link to="/" className="navbar-logo">
-        Brew Showcase
-      </Link>
+    <nav className={styles.navbar}>
+      <div className={styles.navContainer}>
 
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/add-product">Add Product</Link>
+        {/* Coffee shop brand */}
+        <Link to="/" className={styles.brand}>
+          <img
+            src="https://images.unsplash.com/photo-1447933601403-0c6688de566e"
+            alt="Coffee beans"
+            className={styles.bean}
+          />
+
+          <span>Brew Showcase</span>
+
+          <img
+            src="https://images.unsplash.com/photo-1559056199-641a0ac8b55e"
+            alt="Coffee beans"
+            className={styles.bean}
+          />
+        </Link>
+
+        {/* Navigation links */}
+        <div className={styles.links}>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+
+          <Link to="/products" className={styles.link}>
+            Products
+          </Link>
+
+          <Link to="/add-product" className={styles.link}>
+            Add Product
+          </Link>
+        </div>
+
       </div>
     </nav>
   );
