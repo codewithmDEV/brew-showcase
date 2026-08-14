ea391f1e7d2d37f5c1229809d1b6d03632d701a9
Moha <mohammedduale1738@gmail.com>
2026-08-14 14:08:31 +0300

    Initial Commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";// we import useState from react bcz we need to store our data somewhere
-import styles from "./SearchBar.module.css";//import styles to style the searchbar
+import { useState } from "react";
+import styles from "./SearchBar.module.css";
 
-function SearchBar({ onSearch }) {// we create a Searchbar component that takes a parameter onSearch which is destructed
-    const [searchTerm, setSearchTerm] = useState("");// searchTerm is the currentvalue while setSearchTerm will be used to update it
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState("");
 
-    function handleSearch(value) {//this lives in a child components
+    function handleSearch(value) {
         onSearch(value);
     }
 
@@ -12,13 +12,13 @@ function SearchBar({ onSearch }) {// we create a Searchbar component that takes 
         <input
         type="text"
         className={styles.searchInput}
-        value={searchTerm}//searchTerm is the state variable that holds the current tex It ensures the input always shows what's in searchTerm.
+        value={searchTerm}
         onChange={(e) => {
-            setSearchTerm(e.target.value);//updates the local state
-            handleSearch(e.target.value);//calls handleSearch which the calls the parant onSearch
+            setSearchTerm(e.target.value);
+            handleSearch(e.target.value);
         }}
         placeholder="Search products..."
         />
     );
 }
-export default SearchBar;//export so the file can be used
+export default SearchBar;
